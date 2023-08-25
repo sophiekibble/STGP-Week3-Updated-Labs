c445670452aa715090e8c701f3ab7fa9644a8789
Sophie Kibble <sophie.kibble@sky.uk>
2023-08-25 14:27:54 +0100

    add

--- JS/07_JavaScriptFunctions/starter/src/index.js
+++ JS/07_JavaScriptFunctions/starter/src/index.js
@@ -7,7 +7,64 @@ const movies = [
 ];
 
 // Part 1 - Create a function to find a movie and output it's detail
-        
-        
+function findMovie(movieTitle) {
+    for (let movie of movies) {
+        if (movie.title === movieTitle){
+            console.log('${movie.title} is a film directed by ${movie.director} in $movie.year}');
+
+        }
+        console.log(movies);
+    }
+    console.log(movies);
+}  
+
+findMovie("Star Wars");
+console.log(movies)
+
+let movie = "Thor: Ragnorok";
+console.log(movie)
+
+findMovie(movie);
 
 // Part 2 = Create a function to return a movie object
+
+function returnMovie(movieTitle) {
+    for (let movie of movies) {
+        if (movie.title === movieTitle) {
+            return movie;
+        }
+        console.log(movie);
+    }
+
+    console.log("Any text, any text at all!");
+    return `Movie not found`;
+}
+
+let myMovie = returnMovie("Avengers: Infinity War");
+
+console.log(myMovie);
+
+console.log(`${myMovie.title} is a film directed by ${myMovie.director} released in ${myMovie.year}`);
+
+
+let myOtherMovie = returnMovie("Thor: Ragnorok");
+
+console.log(myOtherMovie);
+
+
+function myMovieDetails(anyMovie) {
+    
+    if (typeof (anyMovie) === 'object') {
+        return `${anyMovie.title} is a film directed by ${anyMovie.director} released in ${anyMovie.year}`;
+    } else {
+        return anyMovie;
+    }
+}
+
+
+console.log(myMovieDetails(myOtherMovie));
+
+console.log(myMovieDetails(returnMovie("Jaws")));
+console.log(myMovieDetails(returnMovie("Star Wars")));
+console.log(myMovieDetails(returnMovie("Top Gun")));
+console.log(myMovieDetails(returnMovie("Avengers: Infinity War")));
